1c5777a59e5139f0b1896bdb19697cf8cdb865a4
Callan Stretton <callanstretton@Callans-MacBook-Pro.local>
2017-10-26 15:14:36 +1300

    commit before merge

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,13 +1,24 @@
 import React from 'react'
 import {HashRouter as Router, Route} from 'react-router-dom'
+
+import Home from './Home'
 import Header from './Header'
+import Footer from './Footer'
+import Languages from './Languages'
 
 const App = () => (
-  <Router>
-    <div className='app-container'>
-      <Route exact path="/" component={Header} />
+  <div className='app-container'>
+    <div className="header">
+      <Header />
     </div>
-  </Router>
+    <div className="container">
+      <Route exact path="/" component={Home} />
+      <Languages />
+    </div>
+    <div className="footer">
+      <Footer />
+    </div>
+  </div>
 )
 
 export default App
--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { HashRouter as Router, Route } from 'react-router-dom'
 
 const Header = () => (
-  
+  <div></div>
 )
 
 export default Header
