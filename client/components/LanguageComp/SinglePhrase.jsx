68b19f3453ed5e2e442b1ec685efda563fa09e5e
dazyang <dazyang.contact@gmail.com>
2017-10-27 14:28:45 +1300

    play button working

--- client/components/LanguageComp/SinglePhrase.jsx
+++ client/components/LanguageComp/SinglePhrase.jsx
@@ -4,6 +4,19 @@ import { connect } from 'react-redux'
 import Sound from 'react-sound'
 
 class SinglePhrase extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      playSound: false
+    }
+  }
+  startSound () {
+    this.setState({playSound: true})
+    setTimeout(this.stopSound.bind(this), 1000)
+  }
+  stopSound () {
+    this.setState({playSound: false})
+  }
   render () {
     return (
 
@@ -12,17 +25,19 @@ class SinglePhrase extends React.Component {
         <p>{this.props.phrase.samoanPhrase}</p>
         <p>{this.props.phrase.soundFile}</p>
 
-        <Sound
+        {this.state.playSound && <Sound
           url={this.props.phrase.soundFile}
           playStatus={Sound.status.PLAYING}
           playFromPosition={0 /* in milliseconds */}
           onLoading={this.handleSongLoading}
           onPlaying={this.handleSongPlaying}
           onFinishedPlaying={this.handleSongFinishedPlaying}
-        />
+        />}
+
+        <button onClick={this.startSound.bind(this)}>Play</button>
       </div>
-    ) 
-}
+    )
+  }
 }
 
 export default connect()(SinglePhrase)
