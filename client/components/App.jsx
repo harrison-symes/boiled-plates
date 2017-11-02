f0b40e51dc0be4c22ed0bab43b2c0ca5d2905d6f
Rajal Kumar <rajalkumar13@hotmail.com>
2017-11-02 16:49:29 +1300

    clean up of edits

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import {HashRouter as Router, Route} from 'react-router-dom'
 import Recipes from './recipes/Recipes'
 import Header from './Header'
-import RecipeForm from './recipes/RecipeForm'
+import Recipes from './Recipes'
 import Challenge from './Challenge'
 // import Login from './Login'
 // import Register from './Register'
@@ -18,7 +18,6 @@ const App = () => (
       <Route path='/register' component={Register} /> */}
       <div className='flex-container'>
         <Route exact path='/' component={Recipes} />
-        <Route path='/form' component={RecipeForm} />
         <Route exact path='/' component={Challenge} />
       </div>
       <Footer />
