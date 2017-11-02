a3dbae4623226c1538a72912d621e09ac2496be7
Rajal Kumar <rajalkumar13@hotmail.com>
2017-11-03 11:13:52 +1300

    state showing on redux tool

--- client/components/RecipesHome.jsx
+++ client/components/RecipesHome.jsx
@@ -4,25 +4,38 @@ import {connect} from 'react-redux'
 
 import {getRecipes} from '../actions/recipes'
 import RecipeList from './recipes/RecipeList'
-// import {getRecipes} from '../actions/recipes'
 
-const Recipes = () => {
-  const recipes = props.recipes
+const renderRecipe = (recipe, key) => {
+  
+}
+
+const Recipes = ({recipes, dispatch}) => {
+  // const recipes = props.recipes
   return (
     <div className='link-recipe'>
       <div className='title-box'>
         Find recipes
       </div>
-      <Link to={`/${props.recipes.id}/recipe`}>
         <div className='recipe-description'>
           <p id='recipe-d'>Add this, drain that, cook it up</p>
         </div>
-      </Link>
+      <div>
+        <Link to='/recipe'>
+         <button onClick={() => dispatch(getRecipes())}>Show Recipes</button>
+         {recipes.map(renderRecipe)}
+        </Link>
+      </div>
     </div>
   )
 }
 
-export default Recipes
+const mapStateToProps = (state) => {
+  return {
+    recipes: state.recipes
+  }
+}
+
+export default connect(mapStateToProps)(Recipes)
 
 // const renderRecipe = (recipe, key) => (
   //   <h1 key={key}>{recipe.text}</h1>
