06fa6035c8329b19994de15784f0ba78ee4a43a8
Richard Johns <richard.johns193@gmail.com>
2017-11-03 10:05:06 +1300

    recipe routes and test completed but two tests are very iffy

--- test/server/recipes-db.test.js
+++ test/server/recipes-db.test.js
@@ -2,7 +2,7 @@
 const request = require('supertest')
 
 const env = require('./test-environment')
-const {getRecipes, getRecipe} = require('../../server/db/recipes')
+const {getRecipes, getRecipe, addRecipe, editRecipe} = require('../../server/db/recipes')
 
 // Manage the test database
 
@@ -20,7 +20,7 @@ afterEach(() => env.cleanup(testDb))
 test('read recipes db', () => {
   return getRecipes(testDb)
     .then(recipes => {
-      expect(recipes.length).toBe(1)
+      expect(recipes.length).toBe(4)
       expect(recipes[0].hasOwnProperty('name')).toBeTruthy()
       expect(recipes[0].hasOwnProperty('instructions')).toBeTruthy()
       expect(recipes[0].hasOwnProperty('image')).toBeTruthy()
@@ -30,11 +30,38 @@ test('read recipes db', () => {
 })
 
 test('read recipe db', () => {
-  return getRecipe(1, testDb)
+  return getRecipe(2, testDb)
     .then(recipe => {
       expect(recipe.length).toBe(1)
-      expect(recipe[0].id).toBe(1)
-      expect(recipe[0].name).toBe('Rhubarb Soup')
+      expect(recipe[0].id).toBe(2)
+      expect(recipe[0].name).toBe('mushyroom spud')
     })
 })
 
+test('read specific recipe db', () => {
+  return getRecipe(2, testDb)
+    .then(recipe => {
+      expect(recipe.length).toBe(1)
+      expect(recipe[0].id).toBe(2)
+      expect(recipe[0].name).toBe('mushyroom spud')
+    })
+})
+
+// NOT SURE WHY RETURNS [5] //
+
+test('add recipe to db', () => {
+  let r = {name: 'Honey Sandwich', instructions: 'Spread butter and honey on bread', image: '', ingredients: 'bread, honey, butter', profile_id: 4}
+  return addRecipe(r, testDb)
+    .then(recipe => {
+      expect(recipe[0]).toBe(5)
+    })
+})
+// NOT SURE WHY RETURNS 1 //
+
+test('edit recipe in db', () => {
+  let r = {id: 4, name: 'Jam and Peanut Butter Sandwich', instructions: 'Spread peanut butter and jam on bread', image: '', ingredients: 'bread, jam, peanut butter', profile_id: 4}
+  return editRecipe(r.id, r, testDb)
+    .then(recipe => {
+      expect(recipe).toBe(1)
+    })
+})
