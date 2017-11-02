24aa5550e4ed666c551920875b5420b4fc8520b1
dazyang <dazyang.contact@gmail.com>
2017-11-02 15:34:21 +1300

    update App.jsx

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -3,8 +3,8 @@ import {HashRouter as Router, Route} from 'react-router-dom'
 import Header from './Header'
 import Recipes from './Recipes'
 import Challenge from './Challenge'
-// import Login from './Login'
-// import Register from './Register'
+import Login from './Login'
+import Register from './Register'
 
 import Footer from './Footer'
 
@@ -13,8 +13,8 @@ const App = () => (
   <Router>
     <div className='app'>
       <Header />
-      {/* <Route path='/login' component={Login} />
-      <Route path='/register' component={Register} /> */}
+      <Route path='/login' component={Login} />
+      <Route path='/register' component={Register} />
       <div className='flex-container'>
         <Route exact path='/' component={Recipes} />
         <Route exact path='/' component={Challenge} />
