3395eeb098a88bc455fb6c3a6f08fae6fbc0a600
dazyang <dazyang.contact@gmail.com>
2017-11-03 21:32:05 +1300

    practice

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const path = require('path')
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
-// const request = require('superagent')
+const request = require('superagent')
 
 const recipes = require('./routes/recipes')
 const profiles = require('./routes/profiles')
@@ -10,19 +10,19 @@ const auth = require('./routes/auth')
 
 const server = express()
 
-// server.use('api/v1/searchfood2fork/:id', (req, res) => {
-//   const id = req.params.id
-//   request
-//     .get(`http://food2fork.com/api/get?key=04a1bbc8b9e4709129e5f7455aa51d17&rId=${id}`)
-//     .end((err, res) => {
-//       if (err) {
-//         callback(err)
-//         console.log(err)
-//       } else {
-//         res.json(res.body)
-//       }
-//     })
-// })
+server.use('api/v1/searchfood2fork/:id', (req, res) => {
+  const id = req.params.id
+  request
+    .get(`http://food2fork.com/api/get?key=04a1bbc8b9e4709129e5f7455aa51d17&rId=${id}`)
+    .end((err, res) => {
+      if (err) {
+        callback(err)
+        console.log(err)
+      } else {
+        res.json(res.body)
+      }
+    })
+})
 
 // http://food2fork.com/api/get?key=04a1bbc8b9e4709129e5f7455aa51d17&rId=35120
 
@@ -36,6 +36,5 @@ server.use('/api/auth', auth)
 server.use('/api/recipes', recipes)
 
 server.use('/api/profiles', profiles)
-
 
 module.exports = server
