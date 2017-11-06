6c3cb16105be90bb8cdb609683726dbd42819de2
dazyang <dazyang.contact@gmail.com>
2017-11-06 19:49:59 +1300

    Don rocks

--- seeds/populateUsers.js
+++ seeds/populateUsers.js
@@ -1,15 +1,32 @@
+var bcrypt = require('bcrypt')
+
+const userNames = ['aaron', 'daz', 'skye', 'rajal', 'ramsey']
+
+// This is not your typical seed file because we need to create
+// the hashes for each user's password and creating them is an
+// async operation.
 
 exports.seed = function (knex, Promise) {
-  // Deletes ALL existing entries
+  const insertUsers = insert.bind(null, knex)
   return knex('users').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('users').insert([
-        {id: 1, username: 'aaron', hash: 'aaron'},
-        {id: 2, username: 'daz', hash: 'daz'},
-        {id: 3, username: 'skye', hash: 'skye'},
-        {id: 4, username: 'rajal', hash: 'rajal'},
-        {id: 5, username: 'ramsey', hash: 'ramsey'}
-      ])
-    })
+    .then(createHashes)
+    .then(getUserObjects)
+    .then(insertUsers)
+}
+
+function createHashes () {
+  const hashPromises = userNames.map((user, i) => {
+    return bcrypt.hash(user, 12)
+  })
+  return Promise.all(hashPromises)
+}
+
+function getUserObjects (hashes) {
+  return userNames.map((user, i) => {
+    return {id: i + 1, username: user, hash: hashes[i]}
+  })
+}
+
+function insert (knex, users) {
+  return knex('users').insert(users)
 }
--- server/db/profiles.js
+++ server/db/profiles.js
@@ -1,11 +1,28 @@
 const defaultConn = require('./connection')
 
-const getProfiles = (testConn) => {
+const getProfile = (id, testConn) => {
   const db = testConn || defaultConn
-  return db('profiles')
-    .select()
+  return db('users')
+    .join('profiles', 'profiles.user_id', 'users.id')
+    .join('recipes', 'recipes.user_id', 'users.id')
+    .join('user_progress', 'user_progress.user_id', 'users.id')
+    .select('users.id as userId', 'profiles.firstname', 'profiles.lastname', 'profiles.email', 'recipes.name', 'recipes.instructions', 'recipes.ingredients', 'recipes.image', 'recipes.id as recipeId', 'user_progress.post_type_id')
+    .where({
+      'users.id': id,
+      'recipes.user_id': id,
+      'user_progress.user_id': id
+    })
+    // .then(formatProfile)
 }
 
+// function formatProfile (objects) {
+//   const profile = {
+//     firstname: objects[0].firstname,
+//     lastname: objects[0].lastname
+//   }
+//   return profile
+// }
+
 module.exports = {
-  getProfiles
+  getProfile
 }
