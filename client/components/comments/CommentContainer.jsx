9b75074da7d7f41a192d8f1a87e18a7bc5309421
aaron-macdonald <aaronmac989@gmail.com>
2017-11-07 21:32:39 +1300

    comment adding

--- client/components/comments/CommentContainer.jsx
+++ client/components/comments/CommentContainer.jsx
@@ -6,6 +6,8 @@ import SingleComment from './SingleComment'
 const CommentContainer = (props) => {
   console.log(props)
   const comments = props.comments
+  // const recipeId = props.
+  
   return (
     <div className='comment'>
       {comments.map(comment => {
